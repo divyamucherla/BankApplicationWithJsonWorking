7baaaca61dcd6a62f0861ca49e8b7bec255f5bb3
suresh0523 <suresh.meenege@gmail.com>
2019-07-01 11:27:17 +0000

    first commit

---
+++ src/main/java/com/hcl/bankcustomer/pojo/Customer.java
@@ -0,0 +1,64 @@
+package com.hcl.bankcustomer.pojo;
+
+public class Customer {
+
+	private int id;
+	private String name;
+	private String role;
+	private int phoneNumber;
+	private String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Customer() {
+
+	}
+
+	public Customer(int id, String name, String role, int phoneNumber, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.role = role;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+
+}
